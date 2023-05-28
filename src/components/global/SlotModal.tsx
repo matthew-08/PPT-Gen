13a9c3227ec901ad18a177a99ee1cfe76a291ddf
Matthew <matthewjcrosby1@gmail.com>
2023-05-28 16:16:53 +0700

    [FEAT]: Slot modal

---
+++ src/components/global/SlotModal.tsx
@@ -0,0 +1,45 @@
+import {
+  Button,
+  ModalOverlay,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalContent,
+  Modal,
+  UseDisclosureReturn,
+  Flex,
+} from '@chakra-ui/react';
+import { ReactNode } from 'react';
+
+type Props = {
+  disclosureState: {
+    [K in keyof UseDisclosureReturn as K extends 'isOpen' | 'onOpen' | 'onClose'
+      ? K
+      : never]: UseDisclosureReturn[K];
+  };
+  modalHeader: string;
+  children: ReactNode;
+};
+
+function SignInModal({ disclosureState, modalHeader, children }: Props) {
+  const { isOpen, onClose, onOpen } = disclosureState;
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} motionPreset="slideInBottom">
+      <ModalOverlay />
+      <ModalContent borderRadius="20px">
+        <ModalCloseButton />
+        <ModalBody
+          as={Flex}
+          flexDir="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <ModalHeader fontSize="3rem">{modalHeader}</ModalHeader>
+          {children}
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+}
+
+export default SignInModal;
