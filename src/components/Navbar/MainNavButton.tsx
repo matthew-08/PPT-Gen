6ab683e361fdd7a217804ddfa5bd9dc065ea6259
Matthew <matthewjcrosby1@gmail.com>
2023-06-07 12:39:31 +0700

    [FEAT]: useNavigate hook in conditional nav button

--- src/components/Navbar/MainNavButton.tsx
+++ src/components/Navbar/MainNavButton.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { ButtonGroup, Button } from '@chakra-ui/react';
+import { ButtonGroup, Button, Text, Image } from '@chakra-ui/react';
 import { BiUserCircle } from 'react-icons/bi';
+import { useNavigate } from 'react-router-dom';
 import useAuth from '../../hooks/useAuth';
+import { APP_IMAGES } from '../../utils/images';
 
 type Props = {
   onOpen: () => void;
@@ -13,20 +15,32 @@ function MainNavButton({ onOpen }: Props) {
       authStatus: { loggedIn },
     },
   } = useAuth();
+  const navigate = useNavigate();
   return (
     <ButtonGroup>
-      <Button
-        size="lg"
-        padding="1.5rem"
-        fontSize="1.5rem"
-        onClick={onOpen}
-        _hover={{
-          color: '#553C9A',
-        }}
-        rightIcon={<BiUserCircle size={30} />}
-      >
-        Sign In
-      </Button>
+      {!loggedIn ? (
+        <Button
+          size="lg"
+          padding="1.5rem"
+          fontSize="1.5rem"
+          onClick={onOpen}
+          _hover={{
+            color: '#553C9A',
+          }}
+          rightIcon={<BiUserCircle size={30} />}
+        >
+          Sign In
+        </Button>
+      ) : (
+        <Button
+          size="lg"
+          fontSize="1.5rem"
+          onClick={() => navigate('/contact')}
+          leftIcon={<Image src={APP_IMAGES.presentation} width="20px" />}
+        >
+          My Templates
+        </Button>
+      )}
     </ButtonGroup>
   );
 }
--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,9 +1,11 @@
 import diagram from '../assets/images/diagram.png';
 import ppt from '../assets/images/ppt.svg';
+import presentation from '../assets/images/presentation.svg';
 
 const APP_IMAGES = {
   appDiagram: diagram,
   pptIcon: ppt,
+  presentation,
 };
 
 export { APP_IMAGES };
